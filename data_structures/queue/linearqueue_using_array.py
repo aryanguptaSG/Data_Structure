b47eb0580e6acb999770ad4c71dbdead6d2fa1d7
aryanguptaSG <aryan31102001@gmail.com>
2020-08-28 06:59:02 +0530

    Update linearqueue_using_array.py

--- data_structures/queue/linearqueue_using_array.py
+++ data_structures/queue/linearqueue_using_array.py
@@ -1,56 +1,61 @@
-class queue():
-	"""docstring for queue"""
-	def __init__(self, size=5):
-		self.size=size
-		self.rear=-1
-		self.front=0
-		self.arr=size*[None]
+from os import system, name
+from time import sleep
 
-	def push(self,value):
-		if(self.rear==self.size-1):
-			print("queue is full")
-			return 0
-		else:
-			self.rear+=1
-			self.arr[self.rear]=value
-			print(value, " is added is queue")
-			print("front ",self.front)
-			print("rear ",self.rear)
-
-	def pop(self):
-		if(self.front>self.rear):
-			print("queue is empty")
-			return 0
-		else:
-			print(self.arr[self.front], " is poped")
-			self.front+=1
-			print("front ",self.front)
-			print("rear ",self.rear)
-
-	def display(self):
-		for i in range(self.front,self.rear+1):
-			print(self.arr[i],"\n")
+# this function is created to clear the screen and it doesn't have to do anything with queue
 
 
-		
-list = queue(10)
-list.push(5)
-list.push(6)
-list.push(7)
-list.push(54)
-list.push(53)
-list.push(52)
-list.display()
-list.pop()
-list.pop()
-list.pop()
-list.display()
-list.pop()
-list.pop()
-list.pop()
-list.pop()
-list.push(54)
-list.push(53)
-list.push(52)
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
 
+class Queue():
+    def __init__(self, size=5):
+        self.Max_size = size
+        self.rear = -1
+        self.front = 0
+        self.arr = size * [None]
+
+    def insertion(self, value):
+        if(self.rear == self.Max_size - 1):
+            print("Queue is full!")
+        else:
+            self.rear += 1
+            self.arr[self.rear] = value
+
+    def deletion(self):
+        if(self.front > self.rear):
+            print("Queue is empty!")
+        else:
+            value = self.arr[self.front]
+            self.front += 1
+            return value
+
+    def __str__(self):
+        s = "Queue : "
+        if self.front < self.rear or self.front == self.rear and self.front != -1:
+            for i in range(self.front, self.rear + 1):
+                s += str(self.arr[i]) + " "
+        elif self.rear == -1:
+            s = "\nQueue Empty!"
+        return s
+
+
+list = Queue()
+while True:
+    print("Enter the operation you want to perform:")
+    print("Enter 1 to insertion")
+    print("Enter 2 to deletion")
+    print("Enter 3 to exit")
+    option = int(input("Enter your choice:"))
+    if option == 1:
+        list.insertion(int(input("Enter the value you want to insertion:")))
+    if option == 2:
+        list.deletion()
+    if option == 3:
+        break
+    print("\n", "    ", list)
+    sleep(1.5)
+    clear()
